dcae31fb6021052e3f9475928f4a746104ae1723
kimdoenyi <kjs4082@naver.com>
2022-01-04 21:21:21 +0900

    인터페이스 정적메서드 예제

---
+++ src/InterfaceTest.java
@@ -0,0 +1,5 @@
+public interface InterfaceTest {
+    static void printName() {
+        System.out.println("My name is Kimdoenyi");
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        // 실행시 "My name is Kimdoenyi" 가 출력된다.
+        InterfaceTest.printName();
+    }
+}
