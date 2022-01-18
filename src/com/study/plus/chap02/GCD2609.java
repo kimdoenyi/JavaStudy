3c6c657fba57adf58da54b496cba231c2cb2953a
김진성 <kjs4082@naver.com>
2022-01-18 15:26:07 +0900

    서로소 최대공약수 구할때 오류있었음

--- src/com/study/plus/chap02/GCD2609.java
+++ src/com/study/plus/chap02/GCD2609.java
@@ -8,41 +8,18 @@ public class GCD2609 {
 
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int gcdRes = 0;
-        int lcm = 0;
-
-        if(a > b) {
-            gcdRes = gcd(a,b);
-        } else {
-            gcdRes = gcd(b, a);
-        }
-
-        if(gcdRes == 0) {
-            gcdRes = lcm = a*b;
-        } else {
-            lcm = a * b /gcdRes;
-        }
+        int gcdRes = gcd(a, b);
+        int lcm = a*b/gcdRes;
 
         System.out.println(gcdRes);
         System.out.println(lcm);
     }
 
     static int gcd(int a, int b) {
-
-        int res = 0;
-
-        while(true) {
-            res = a % b;
-
-            if(res == 0) {
-                return 0;
-            } else if(b % res == 0) {
-                return res;
-            } else {
-                a = b;
-                b = res;
-            }
-
+        if(b == 0) {
+            return a;
+        } else {
+            return gcd(b, a%b);
         }
     }
 
