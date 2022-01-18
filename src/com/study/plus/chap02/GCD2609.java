8e2571f30c8ddacdace2dfb150f61fd2df0f6769
김진성 <kjs4082@naver.com>
2022-01-18 15:28:06 +0900

    대소비교 안해도되는 이유 주석추가

--- src/com/study/plus/chap02/GCD2609.java
+++ src/com/study/plus/chap02/GCD2609.java
@@ -19,6 +19,7 @@ public class GCD2609 {
         if(b == 0) {
             return a;
         } else {
+            // a,b 대소비교를 안해도되는 이유 -> 어차피 작은수%큰수=작은수가 되니까 ..
             return gcd(b, a%b);
         }
     }
